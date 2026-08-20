9f86505198451a202d2b60b059c2a86483c0b7a5
vera0406 <tianxin0406@gmail.com>
2026-08-20 17:01:37 +0800

    feat: initialize project structure with site constants, Navbar component, and media assets

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <a href="#home" className="flex items-center gap-2">
+                    <p>Velvet Pour</p>
+                </a>
+
+                <ul>
+                    {navLinks.map((link) => (
+                        <li key={link.id}>
+                            <a href={`#${link.id}`}> {link.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
