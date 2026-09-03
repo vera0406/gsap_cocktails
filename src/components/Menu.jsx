571f677722b67d769a9c5772bf236c24ca6ca58e
vera0406 <tianxin0406@gmail.com>
2026-09-03 14:24:21 +0800

    feat: initialize project structure with main application layout and Tailwind CSS styling

---
+++ src/components/Menu.jsx
@@ -0,0 +1,14 @@
+const menu = () => {
+    return (
+        <section id="menu" aria-labelledby="menu-heading">
+            <img src="/images/slider-left-leaf.png" alt="left-leaf" id="m-left-leaf" />
+            <img src="/images/slider-right-leaf.png" alt="right-leaf" id="m-right-leaf" />
+
+            <h2 id="menu-heading" className="sr-only">
+                Cocktail Menu
+            </h2>
+        </section>
+    )
+}
+
+export default menu
